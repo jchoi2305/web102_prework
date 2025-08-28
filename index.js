ae3af371dfda979ac22c5468c85c1ec46dd2dfa0
jchoi2305 <jchoi2156@gmail.com>
2025-08-28 09:45:00 -0400

    challenge 4

--- index.js
+++ index.js
@@ -62,22 +62,33 @@ addGamesToPage(GAMES_JSON);
 */
 
 // grab the contributions card element
-const contributionsCard = document.getElementById("num-contributions");
+const contributionsCard = document.getElementById("num-contributions"); 
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (total, game) => {
+    return total + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<p>${totalContributions.toLocaleString()}</p>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+// reduce()
+const totalPledged = GAMES_JSON.reduce( (total, game) => {
+    return total + game.pledged;
+}, 0);
+
 // set inner HTML using template literal
+raisedCard.innerHTML = `<p>$${totalPledged.toLocaleString()}`;
 
-
+// -> displaying the total # of games in the total games card
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+// displaying the length of GAMES_JSON because it = number of games
+gamesCard.innerHTML = `<p>${GAMES_JSON.length}</p>`;
 
 
 /*************************************************************************************
